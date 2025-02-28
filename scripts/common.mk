ffe09fffce720fd407d5ef8391a07046b0c5762d
uchan-nos <uchan@uchan.net>
2025-02-28 11:53:20 +0900

    led_patternに対してテストベンチを追加

--- scripts/common.mk
+++ scripts/common.mk
@@ -9,20 +9,28 @@ GWSH = $(GWIDE_BINDIR)/gw_sh.exe
 GWPROG = $(scripts_dir)/gw_prog.sh
 MAKE_BUILD_TCL = $(scripts_dir)/make_build_tcl.sh
 
+SRCS_HDL = $(filter %.v %.sv,$(SRCS))
+
 all: impl/pnr/project.fs
 
 impl/pnr/project.fs: build.tcl Makefile
-	${GWSH} build.tcl
+	$(GWSH) build.tcl
 
-build.tcl: ${SRCS} Makefile
-	${MAKE_BUILD_TCL} ${SRCS} > build.tcl
+build.tcl: $(SRCS) Makefile
+	$(MAKE_BUILD_TCL) $(SRCS) > build.tcl
 
-.PHONY: flash sram clean
+sim.exe: $(TESTBENCH) $(SRCS_HDL) Makefile
+	iverilog -g2012 -o $@ $(TESTBENCH) $(SRCS_HDL)
+
+.PHONY: flash sram clean sim
 flash: impl/pnr/project.fs Makefile
-	${GWPROG} flash impl/pnr/project.fs
+	$(GWPROG) flash impl/pnr/project.fs
 
 sram: impl/pnr/project.fs Makefile
-	${GWPROG} sram impl/pnr/project.fs
+	$(GWPROG) sram impl/pnr/project.fs
 
 clean: Makefile
-	rm -rf build.tcl impl
+	rm -rf build.tcl impl sim.exe
+
+sim: sim.exe Makefile
+	./sim.exe
---
+++ verilog_practice/led_pattern/main_tb.sv
@@ -0,0 +1,23 @@
+module main_tb;
+
+logic sys_clk, rst_n_raw;
+logic [5:0] onboard_led;
+
+main main(.*);
+
+// 5 単位時間ごとにクロックを反転 => 10 単位時間で 1 クロック
+always #5 begin
+  sys_clk = ~sys_clk;
+end
+
+initial begin
+  sys_clk <= 0;
+  rst_n_raw <= 0;
+  $timeformat(0, 0, "", 5);
+  $monitor("%t led=%b", $time, onboard_led);
+
+  #100
+  $finish();
+end
+
+endmodule
