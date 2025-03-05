47f77db695f483c366916eb1a75c0632832aa69e
uchan-nos <uchan@uchan.net>
2025-03-05 10:06:19 +0900

    配線出力版の led_blink_wire を追加

--- verilog_practice/led_blink_wire/main.sv
+++ verilog_practice/led_blink_wire/main.sv
@@ -1,5 +1,5 @@
 /*
- * 0.5 秒ごとに LED のパターンを反転させる回路
+ * 0.5 秒ごとに LED のパターンを反転させる回路（配線出力版）
  *
  * Tang Nano 9K での動作の様子を YouTube で公開しています。
  * https://www.youtube.com/watch?v=d-PA88KCTPU
@@ -7,11 +7,11 @@
 module main(
   input sys_clk,
   input rst_n_raw,
-  output logic [5:0] onboard_led
+  output [5:0] onboard_led
 );
 
 logic rst_n;
-logic [23:0] counter;
+logic [24:0] counter;
 
 always @(posedge sys_clk) begin
   rst_n <= rst_n_raw;
@@ -20,17 +20,13 @@ end
 always @(posedge sys_clk, negedge rst_n) begin
   if (~rst_n)
     counter <= 0;
-  else if (counter >= 13_500_000 - 1)
+  else if (counter >= 27_000_000 - 1)
     counter <= 0;
   else
-    counter <= counter + 24'd1;
+    counter <= counter + 25'd1;
 end
 
-always @(posedge sys_clk, negedge rst_n) begin
-  if (~rst_n)
-    onboard_led <= 6'b111001;
-  else if (counter == 0)
-    onboard_led <= ~onboard_led;
-end
+// 出力を配線（継続代入）で行う
+assign onboard_led = counter < 13_500_000 ? 6'b101001 : 6'b010110;
 
 endmodule
